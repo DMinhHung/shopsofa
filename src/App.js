14e03b8f472d89536c749dbd509914dad7571ce6
daominhhung <daominhhung2203@gmail.com>
2024-04-10 19:55:39 +0700

    Push Frontend lần 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "./container/user/Index";
+import Shop from "./container/user/Shop";
+import About from "./container/user/About";
+import Services from "./container/user/Services";
+import Blog from "./container/user/Blog";
+import Contact from "./container/user/Contact";
 
-function App() {
+//Import CSS and JS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/assets/css/style.css";
+import "../src/assets/css/tiny-slider.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "../src/assets/js/custom.js";
+// import "../src/assets/js/tiny-slider.js";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
